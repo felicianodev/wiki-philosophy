aeb503f820ed8f38e708ed8ca393620bf0c98d04
felicianodev <60105190+felicianodev@users.noreply.github.com>
2020-11-12 10:24:17 +0100

    Update README.js

--- README.js
+++ README.js
@@ -1,13 +1,15 @@
+This is a script to automatically play the "Wikipedia:getting to philosophy" game. 
 
+more information: https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
+
+In theory starting from any term and clicking the first non-parenthesized should eventually lead to the page for Philosophy.
+This script uses webdriverio and selenium to do that automatically
+
+INSTALL DEPENDENCIES:
 npm i webdriverio
 npm i selenium-standalone
 ./node_modules/selenium-standalone/bin/selenium-standalone install
 ./node_modules/selenium-standalone/bin/selenium-standalone start
+
+START THE SCRIPT:
 node wiki_philosophy.js
-
-
-//TODO
-//if there's a parentheses around the link, do not click it
-//containsanumber method
-//
-`	`
